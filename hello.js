a712bbe0f4fa0a245fb4909e54e44e1dbd6300ec
Sreelakshminarayanan <sreelakshminarayanan@Sreelakshminarayanans-MacBook-Pro.local>
2018-04-22 18:11:36 +0530

    include request handler

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+
+app.get('/nagaraj', (req, res) => res.send('<html><h2>Hello World!</h2></html>'))
+
+app.listen(process.env.PORT  || 3000, () => console.log('Example app listening on port 3000!'))
